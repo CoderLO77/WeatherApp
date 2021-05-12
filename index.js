4b4023ed0e56cc33ed18d55d0a86032f1a6b6bc0
CoderLO77 <80711122+CoderLO77@users.noreply.github.com>
2021-05-12 00:50:36 -0500

    Update index.js
    work in progress effort to update temperature functions. Also an attempt to get c and f links working properly.

--- index.js
+++ index.js
@@ -40,13 +40,37 @@ let currentTime = document.querySelector("#current-time");
 let currentDate = new Date();
 currentTime.innerHTML = formatDate(currentDate);
 
+function getForecast(coordinates){
+  let apiKey="0ab851116d25275147c4636ad9ba56ee";
+  let apiurl=`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showTemperature(response) {
   let temperature = Math.round(response.data.main.temp);
   let temperatureElement = document.querySelector("#temp");
   temperatureElement.innerHTML = `${temperature}`;
   let h2 = document.querySelector("#city");
   h2.innerHTML = `${response.data.name}`;
-}
+ 
+  celsiusTemperature=response.data.main.temp
+  
+  let humidityElement=document.querySelector("#humidity");
+  let windElement=document.querySelector("#wind");
+  let descriptionElement=document.querySelector("#description");
+  let iconElement=document.querySelector("#icon");
+  let forecastElement=document.querySelector("#forecast");
+  humidityElement.innerHTML=response.data.main.humidity;
+  windElement.innerHTML= Math.round(response.data.wind.speed);
+  description.innerHTML= response.data.weather[0].description;
+   iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord)
+   }
 
 function showPosition(position) {
   let latitude = position.coords.latitude;
@@ -64,18 +88,44 @@ function handleSubmit(event) {
   searchInput.innerHTML = cityInput.value;
   search(cityInput.value);
 }
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", handleSubmit);
+
 
 function search(city) {
   let apiKey = "0ab851116d25275147c4636ad9ba56ee";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(`${apiUrl}&appId=${apiKey}`).then(showTemperature);
 }
-
+  
 function getLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(showPosition);
 }
 let currentLocation = document.querySelector("#current-location-button");
 currentLocation.addEventListener("click", getLocation);
+
+function showCelsiusTemperature(event){
+  event.preventDefault();
+   let temperatureElement=document.querySelector("#temp");
+  let celsiusTemperature=(fahrenheitTemperature-32)*5/9;
+  temperatureElement.innerHTML=Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature= null;
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+function showfahrenheitTemperature(event){
+  event.preventDefault();
+  let temperatureElement=document.querySelector("#temp");
+  temperatureElement.innerHTML=Math.round(fahrenheitTemperature);
+}
+
+
+let celsiusLink=document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", showCelsiusTemperature);
+
+let fahrenheitLink=document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", showFahrenheitTemperature);
+
+search("Austin");
